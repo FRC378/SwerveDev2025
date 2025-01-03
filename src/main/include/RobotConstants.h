27b452fcc7b0c56dd0af4f3a33037a58ed6c06a5
K.Sielski <kris@sielski.com>
2025-01-03 14:26:01 -0500

    Swerve Madule alignment

--- src/main/include/RobotConstants.h
+++ src/main/include/RobotConstants.h
@@ -19,7 +19,7 @@
 #define FRONTLEFT_DRIVE_CAN_ID      11 
 #define FRONTLEFT_TURN_CAN_ID       12 
 #define FRONTLEFT_ENCODER_ID        0 
-#define FRONTLEFT_ENCODER_OFFSET    (298.3)
+#define FRONTLEFT_ENCODER_OFFSET    (299.2)
 //-------------------------------------------
 #define FRONTRIGHT_DRIVE_CAN_ID     21
 #define FRONTRIGHT_TURN_CAN_ID      22
@@ -29,10 +29,10 @@
 #define BACKLEFT_DRIVE_CAN_ID       14
 #define BACKLEFT_TURN_CAN_ID        24
 #define BACKLEFT_ENCODER_ID         3
-#define BACKLEFT_ENCODER_OFFSET     (315.3)
+#define BACKLEFT_ENCODER_OFFSET     (316.5)
 //-------------------------------------------
 #define BACKRIGHT_DRIVE_CAN_ID      31
 #define BACKRIGHT_TURN_CAN_ID       32
 #define BACKRIGHT_ENCODER_ID        2
-#define BACKRIGHT_ENCODER_OFFSET    (109.5)
+#define BACKRIGHT_ENCODER_OFFSET    (112.6)
 //-------------------------------------------
