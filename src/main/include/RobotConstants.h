92a051e886ae6fa0da020d86fc98c4ad0ba28aef
K.Sielski <kris@sielski.com>
2025-03-06 17:46:16 -0500

    Test swerve on new robot

--- src/main/include/RobotConstants.h
+++ src/main/include/RobotConstants.h
@@ -18,21 +18,21 @@
 //-------------------------------------------
 #define FRONTLEFT_DRIVE_CAN_ID      32
 #define FRONTLEFT_TURN_CAN_ID       31 
-#define FRONTLEFT_ENCODER_ID        0 
-#define FRONTLEFT_ENCODER_OFFSET    (299.2)
+#define FRONTLEFT_ENCODER_ID        2 
+#define FRONTLEFT_ENCODER_OFFSET    (220.2)
 //-------------------------------------------
 #define FRONTRIGHT_DRIVE_CAN_ID     33
 #define FRONTRIGHT_TURN_CAN_ID      34
-#define FRONTRIGHT_ENCODER_ID       1
-#define FRONTRIGHT_ENCODER_OFFSET   (310.5)
+#define FRONTRIGHT_ENCODER_ID       0
+#define FRONTRIGHT_ENCODER_OFFSET   (28.9)
 //-------------------------------------------
 #define BACKLEFT_DRIVE_CAN_ID       24
 #define BACKLEFT_TURN_CAN_ID        23
 #define BACKLEFT_ENCODER_ID         3
-#define BACKLEFT_ENCODER_OFFSET     (316.5)
+#define BACKLEFT_ENCODER_OFFSET     (319.4)
 //-------------------------------------------
 #define BACKRIGHT_DRIVE_CAN_ID      21
 #define BACKRIGHT_TURN_CAN_ID       22
-#define BACKRIGHT_ENCODER_ID        2
-#define BACKRIGHT_ENCODER_OFFSET    (112.6)
+#define BACKRIGHT_ENCODER_ID        1
+#define BACKRIGHT_ENCODER_OFFSET    (114.4)
 //-------------------------------------------
